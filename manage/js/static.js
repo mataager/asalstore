ef301e619a31a30bd7e4aa6c04b1d386b95f614d
mataager <omarbussmo077@gmail.com>
2024-11-03 13:40:05 +0200

    Add files via upload

---
+++ manage/js/static.js
@@ -0,0 +1,9 @@
+const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const uid = localStorage.getItem("uid");
+const matager_percentage = 0.05;
+const clientId = "2ec1d3a8cdb5df7";
+//for handling the page title
+const storetitle = "Asal Store";
+document.getElementById("title").innerHTML = storetitle;
+//
